8aa07ade43b7d5a7d4989295a58048d6087f2c57
Tamirlan <tamirlan55@gmail.com>
2022-09-15 15:55:14 +0600

    Добавление запроса в table_user.py

--- table_user.py
+++ table_user.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, String, Integer
-from database import Base
+from sqlalchemy import Column, String, Integer, func
+from database import Base, engine, SessionLocal
+
+
 
 class User(Base):
     __tablename__ = "user"
@@ -11,3 +13,10 @@ class User(Base):
     country = Column(String)
     city = Column(String)
     os = Column(String)
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    session = SessionLocal()
+
+    result = session.query(User.country, User.os, func.count(User.id)).filter(User.exp_group == 3).group_by(User.country).group_by(User.os).having(func.count(User.id) > 100).order_by(func.count(User.id).desc()).all()
+    print(result)
