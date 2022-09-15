b2aff230536e5064363e248197fa4447d287d045
Tamirlan <tamirlan55@gmail.com>
2022-09-15 17:39:14 +0600

    Добавлен файл schema.py, в нем описаны модели pydantic для валидации данных

---
+++ schema.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class UserGet(BaseModel):
+    id: int
+    age: int
+    exp_group: int
+    gender: int
+    source: str
+    country: str
+    city: str
+    os: str
+    class Config:
+        orm_mode = True
+
+class PostGet(BaseModel):
+    id: int
+    text: str
+    topic: str
+    class Config:
+        orm_mode = True
+
+class FeedGet(BaseModel):
+    user_id: int
+    post_id: int
+    time: datetime
+    action: str
+    class Config:
+        orm_mode = True
